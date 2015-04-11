93e8bb07951cd142b04f63121b4dbd79842d6e57
Alistair Lynn <arplynn@gmail.com>
2015-04-11 18:46:34 +0100

    Bump the settings in DesktopLauncher

--- desktop/src/uk/co/alynn/games/ld32warmup/desktop/DesktopLauncher.java
+++ desktop/src/uk/co/alynn/games/ld32warmup/desktop/DesktopLauncher.java
@@ -1,12 +1,22 @@
 package uk.co.alynn.games.ld32warmup.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import uk.co.alynn.games.ld32warmup.LD32WarmupGame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LD32WarmupGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1024;
+        config.height = 640;
+
+        config.resizable = true;
+
+        config.useHDPI = true;
+
+        config.title = "LD32 Warmup";
+
+        new LwjglApplication(new LD32WarmupGame(), config);
+    }
 }
