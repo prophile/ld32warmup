8771a526e46ce88970181651ab2a4bcd8f8a8bee
Alistair Lynn <arplynn@gmail.com>
2015-04-11 20:08:30 +0100

    Make the HTML version palatable

--- html/src/uk/co/alynn/games/ld32warmup/client/HtmlLauncher.java
+++ html/src/uk/co/alynn/games/ld32warmup/client/HtmlLauncher.java
@@ -1,19 +1,22 @@
 package uk.co.alynn.games.ld32warmup.client;
 
+import uk.co.alynn.games.ld32warmup.LD32WarmupGame;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import uk.co.alynn.games.ld32warmup.LD32WarmupGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig () {
+        GwtApplicationConfiguration config = new GwtApplicationConfiguration(1024, 640);
+        config.antialiasing = true;
+        return config;
+    }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new LD32WarmupGame();
-        }
+    @Override
+    public ApplicationListener getApplicationListener () {
+        return new LD32WarmupGame();
+    }
 }
